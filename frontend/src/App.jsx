45d8c7c3fd8946fe6099fc59c8ad57758e108ad9
Garvita Jain <garvitajain1307@gmail.com>
2026-07-18 12:01:45 +0530

    path fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,7 +58,7 @@ function App() {
   path="/company/setup"
   element={!isAuthenticated ? <Navigate to="/" /> : <CompanySetup />}
 />
-      <Route path="/company/setup/success" element={!isAuthenticated ? <Navigate to="/" /> : <CompanySetupSuccess />}/>
+      <Route path="/company/setup/success" element={!isAuthenticated ? <Navigate to="/" /> : <CompanySetUpSuccess />}/>
       <Route path="/dashboard" element={!isAuthenticated ? <Navigate to="/"/> : !admin?.companyId ? <Navigate to="/company/setup"/> : <Dashboard/>}/> 
       <Route path="/billing" element={!isAuthenticated ? <Navigate to="/"/> : !admin?.companyId ? <Navigate to="/company/setup"/> : <Billing/>}/>
  
