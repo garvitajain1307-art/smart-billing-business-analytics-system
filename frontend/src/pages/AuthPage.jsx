adcddebdce13c2eb1c521350fd03011271e65f21
Garvita Jain <garvitajain1307@gmail.com>
2026-07-18 12:36:53 +0530

    error resolved

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState,useEffect } from "react"
 import "./AuthPage.css";
 import {ReceiptIndianRupee,FileText,ChartColumn,ShoppingCart,TrendingUp } from "lucide-react"
 import {useDispatch,useSelector} from "react-redux"
